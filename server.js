d7f0c4ad520abe0946a8003be93f8f5ab49068f8
Thanaphas <5706021632049@fitm.kmutnb.ac.th>
2016-12-11 12:09:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const axios = require('axios')
 const app = express()
-const token = 'EAAXEcK9g0skBAJposuJFuFuc8qGPMLpZA2C848sWZBwRFCfjjdvMRGNd9DSasHHVgdadjRBZCzYgrYDa1FxTVzlDZCSCHyBhHk13nasaVd2JN31vh5m1Kk9uVBXxJqD8m3o41yUuAmAL4d1vX7Dn5vQqJObFY78ZC1PjdNOBG2AZDZD'
+const token = 'EAAQ7MwzNNKUBAKvsan60ka73hfc4SouF9c0anDWeJam00GLZBXdT5Yf40JtbymZBeo0ZAsIu9JgkL7ZA0BTPs9Y4PXvRrczibXIE36hWC33j0JURNwntBmj9hspFEibgAVDdzvK7WdRyvdxMkuYscUKLMvZAaHWDUWKpAHiasMgZDZD'
 app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
   res.send('test test')
 })
 app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === 'koykoy') {
+  if (req.query['hub.verify_token'] === 'weather') {
     res.send(req.query['hub.challenge'])
   }
   res.send('Error, wrong token')
@@ -24,6 +24,21 @@ app.post('/webhook/', function (req, res) {
     let event = req.body.entry[0].messaging[i]
     let sender = event.sender.id
     if (event.message && event.message.text) {
+       var location = event.message.text
+      var weatherEndpoint = 'http://api.openweathermap.org/data/2.5/weather?q=' +location+ '&units=metric&appid=ea5272e74853f242bc0efa9fef3dd9f3'
+      request({
+        url: weatherEndpoint,
+        json: true
+      }, function(error, response, body) {
+        try {
+          var condition = body.main;
+          sendTextMessage(sender, "วันนี้ อุณหภูมิ " + condition.temp + " °C  " + "ความชื้น " + condition.humidity + " % ที่ " + location);
+        } catch(err) {
+          console.error('error caught', err);
+          sendTextMessage(sender, "โปรดใส่ชื่อเมืองให้ถูกต้อง(ยกตัวอย่างเช่น ฺBangkok )");
+        }
+      })
+
       let text = event.message.text
       if (text === 'Generic') {
         sendGenericMessage(sender)
